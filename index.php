865b74a8c37346c6e3f95ab836c5934646fed008
Estudiantes <Estudiantes@Labing70209.udistrital.edu.co>
2017-08-30 18:03:59 -0500

    PAGINA DE PAREJAS color final

--- index.php
+++ index.php
@@ -4,14 +4,12 @@
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
-        <form action="pareja.php">
+    <body style="background-color: red;">
+    <center><form action="pareja.php">
             <input type="text" name="hombre">.<br>.<br>.<br>
             <input type="text" name="mujer">
-            <input type="submit" name="pareja">.<br>.<br>.<br>
-        </form>
-        <?php
-        echo $_REQUEST ["hombre"]." se aman ".$_REQUEST ["mujer"] ;
-        ?>
+            <input type="submit" name="Unir">.<br>.<br>.<br>
+        </form></center>
+        
     </body>
 </html>
